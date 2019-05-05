9b8d234d3cd4b919d433b5fe6aa0a038fab7ba30
u1809 <1473925065@qq.com>
2019-05-05 15:42:39 +0800

    apitext

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\UserModel;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class UserController extends Controller
+{
+    public function user(){
+        $arr = $_GET['id'];
+    }
+
+    public function index(){
+
+    }
+
+}
